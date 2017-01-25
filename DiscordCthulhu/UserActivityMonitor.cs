08125edfb306fc891e64f80c345806d0cfce6d83
Lomztein <lomztein2@gmail.com>
2017-01-25 22:30:32 +0100

    User activity monitor works better.

--- DiscordCthulhu/UserActivityMonitor.cs
+++ DiscordCthulhu/UserActivityMonitor.cs
@@ -118,10 +118,9 @@ namespace DiscordCthulhu {
             }
 
             // This might be heavy on the server during midnights.
-            if (user.HasRole (toAdd[0])) {
-                user.AddRoles (toAdd.ToArray ());
-            }else {
+            if (!user.HasRole (toAdd[0])) {
                 ChatLogger.Log ("Adding role " + toAdd[0].Name + " to user " + user.Name);
+                user.AddRoles (toAdd.ToArray ());
             }
 
             bool missingAny = false;
