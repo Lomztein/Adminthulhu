c945ad4b83ec2c8dc0661218f18d557ccbcf053a
Lomztein <lomztein2@gmail.com>
2017-08-21 01:08:10 +0200

    Maybe if I..

--- AdminthulhuCore/AutoPatcher.cs
+++ AdminthulhuCore/AutoPatcher.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
-using Lomz.ProgramPatcher;
+using System.Net.Http;
+using System.Diagnostics;
 
 namespace Adminthulhu
 {
     public class AutoPatcher : IClockable {
 
-        public static string url = "https://github.com/Lomztein/Adminthulhu/blob/master/Compiled/changelog.txt";
+        public static string url = "https://raw.githubusercontent.com/Lomztein/Adminthulhu/master/Compiled/";
 
         public Task Initialize(DateTime time) {
             return Task.CompletedTask;
@@ -24,8 +25,30 @@ namespace Adminthulhu
         }
 
         public async Task OnMinutePassed(DateTime time) {
-            string changelog = await Patcher.DownloadChangelog (url);
-            Logging.DebugLog (Logging.LogType.BOT, changelog);
+            using (HttpClient client = new HttpClient ()) {
+                string basePath = AppContext.BaseDirectory + "/";
+
+                string localVersion = "";
+                try {
+                    localVersion = SerializationIO.LoadTextFile (basePath + "version.txt") [ 0 ];
+                } catch { }
+
+                string version = await client.GetStringAsync (url + "version.txt");
+
+                if (localVersion != version) {
+                    string changelog = await client.GetStringAsync (url + "changelog.txt");
+
+                    Process patcher = new Process ();
+                    patcher.StartInfo.FileName = AppContext.BaseDirectory + "/patcher/AdminthulhuPatcher.dll";
+                    patcher.StartInfo.CreateNoWindow = false;
+                    patcher.StartInfo.UseShellExecute = true;
+
+                    patcher.StartInfo.Arguments = url + "publish.zip, " + basePath;
+                    patcher.Start ();
+
+                    Environment.Exit (0);
+                }
+            }
         }
 
         public Task OnSecondPassed(DateTime time) {
